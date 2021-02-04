cdc8cff844f5d50f4f6586956ada7d69e7f2be43
Oscar Patricio <ext.oscar.patricio@farfetch.com>
2021-02-04 21:49:14 +0000

    fix: fix example

--- examples/main.tf
+++ examples/main.tf
@@ -2,25 +2,17 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_id" "rg_name" {
-  byte_length = 8
-}
-
-resource "random_id" "vnet_name" {
-  byte_length = 8
-}
-
-resource "random_id" "subnet_name" {
+resource "random_id" "example_id" {
   byte_length = 8
 }
 
 resource "azurerm_resource_group" "example_rg" {
-  name     = "example-${random_id.rg_name.hex}-rg"
+  name     = "example-${random_id.example_id.hex}-rg"
   location = "northeurope"
 }
 
 resource "azurerm_virtual_network" "example_vnet" {
-  name                = "example-${random_id.vnet_name.hex}-vnet"
+  name                = "example-${random_id.example_id.hex}-vnet"
   resource_group_name = azurerm_resource_group.example_rg.name
   address_space       = ["10.10.10.0/24"]
   location            = "northeurope"
@@ -29,7 +21,7 @@ resource "azurerm_virtual_network" "example_vnet" {
 module "subnet" {
   source = "../"
 
-  name                 = "example-${random_id.subnet_name.hex}-subnet"
+  name                 = "example-${random_id.example_id.hex}-subnet"
   resource_group_name  = azurerm_resource_group.example_rg.name
   virtual_network_name = azurerm_virtual_network.example_vnet.name
   address_prefixes     = ["10.10.10.0/28", "10.10.10.16/28"]
